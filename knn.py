1294a72b722b7b258139d85cc1ecb016885adc50
Giovanni Alcantara <me@giovannialcantara.com>
2016-03-23 17:24:23 +0000

    Refactor kNN, add accuracy, update specs

--- knn.py
+++ knn.py
@@ -9,23 +9,14 @@ train_classes = data['train_classes']
 test_classes = data['test_classes']
 
 
-def euclidean_distances(v1, v2):
-    d = ((v1 - v2) ** 2).sum()
-    return np.sqrt(d)
-
-
 def find_modes(lst):
     max_freq = max(map(lst.count, lst))
     modes = [i for i in lst if lst.count(i) == max_freq]
     return list(set(modes))
 
 
-def knn(data, k):
-    # distances = []
-    # for x in train_features:
-    #     distances.append(euclidean_distances(x, data))
-    (train_features - data) ** 2
-    distances = np.sqrt(np.sum(((train_features - data) ** 2), axis=1))
+def classify(v, k):
+    distances = np.sqrt(np.sum(((train_features - v) ** 2), axis=1))
 
     distances = np.array(distances)
     idx = distances.argsort()[::1]
@@ -40,13 +31,16 @@ def knn(data, k):
     else:
         return labels_X[0]
 
-# knn(test_features[999])
 
-confusion_matrix = np.zeros((10, 10))
-actual_classes = test_classes[0]
+def knn(k):
+    confusion_matrix = np.zeros((10, 10))
+    actual_classes = test_classes[0]
 
-for i in range(len(test_features)):
-    predicted_class = knn(test_features[i], 1)
-    confusion_matrix[actual_classes[i]-1][predicted_class-1] += 1
+    for i in range(len(test_features)):
+        predicted_class = classify(test_features[i], k)
+        confusion_matrix[actual_classes[i]-1][predicted_class-1] += 1
 
-print(confusion_matrix)
+    print(confusion_matrix)
+    print("Accuracy: ", np.sum(np.diag(confusion_matrix)) / np.sum(confusion_matrix))
+
+knn(1)
